f4b2d58e9929e5c435ea0052d636935ff9838691
Jacky <flashlib@gmail.com>
2014-05-08 16:36:53 +0800

    Refactor. HeYi.

--- RomansNumber.js
+++ RomansNumber.js
@@ -1,36 +1,37 @@
 function RomansNumber(){
   "use strict";
 
-  var romanSource = ["I"];
-  var baseRoman = {
-    '1':'I',
-    '4':'IV',
-    '5':'V',
-    '9':'IX',
-    '10':'X',
-    '40':'XL',
-    '50':'L',
-    '90':'XC',
-    '100':'C',
-    '400':'CD',
-    '500':'D',
-    '900':'CM',
-    '1000':'M',
+  // var baseRoman = {
+    // '1':'I',
+    // '4':'IV',
+    // '5':'V',
+    // '9':'IX',
+    // '10':'X',
+    // '40':'XL',
+    // '50':'L',
+    // '90':'XC',
+    // '100':'C',
+    // '400':'CD',
+    // '500':'D',
+    // '900':'CM',
+    // '1000':'M',
+//
+  // };
 
-  };
+  var baseNum = [1,5,10,50,100,500,1000];
+  var charator = ['I','V','X','L','C','D','M'];
+
   var obj = {
-    number:0,
     convert: function(num){
-      var basic = baseRoman[num.toString()];
-      var lessbasic = baseRoman[(num+1).toString()];
-      if(basic){
-        return basic;
+      var returnNum;
+      for(var i=0;i<baseNum.length;i++){
+        if(baseNum[i] == num){
+          returnNum = charator[i];
+        }
       }
-      else if(lessbasic){
-        return baseRoman[(1).toString()]+lessbasic;
-      }
+      return returnNum;
     }
-  };
+   };
 
-  return obj;
+   return obj;
 }
