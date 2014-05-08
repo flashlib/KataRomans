ea2a207857288a9dba9f2cee770fc71d5c98f84c
Jacky <flashlib@gmail.com>
2014-05-08 15:17:50 +0800

    add 10 should be X

--- test.js
+++ test.js
@@ -11,3 +11,9 @@ test("5 should be V", function() {
   var roman = romansNumber.convert(5);
   ok("V" === roman, "5 should be V, current: " + roman);
 });
+
+test("10 should be X", function() {
+  var romansNumber = new RomansNumber();
+  var roman = romansNumber.convert(10);
+  ok("X" === roman, "10 should be X, current: " + roman);
+});
