c2d393dc6268875770a73bb37037c842ac6acc88
Jacky <flashlib@gmail.com>
2014-05-08 15:15:05 +0800

    ????????

--- RomansNumber.js
+++ RomansNumber.js
@@ -2,20 +2,21 @@ function RomansNumber(){
   "use strict";
 
   var romanSource = ["I"];
+  var baseRoman = {
+    '1':'I',
+    '5':'V',
+    '10':'X',
+    '50':'L',
+    '100':'C',
+    '500':'D',
+    '1000':'M'
+  };
   var obj = {
     number:0,
     convert: function(num){
-      var romanStr;
-      if(num === 1)
-      {
-        romanStr = 'I';
-      }
-      else{
-        romanStr = 'V';
-      }
-
-      return romanStr;
+      return baseRoman[num.toString()];
     }
   };
+
   return obj;
 }
