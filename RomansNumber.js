25e719075370013616ab93af69b3a5c51767383e
Jacky <flashlib@gmail.com>
2014-05-22 14:51:20 +0800

    ??????????????????1=I. Jacky

--- RomansNumber.js
+++ RomansNumber.js
@@ -1,35 +1,29 @@
 function RomansNumber() {"use strict";
-
-  // var baseNum = [1,5,10,50,100,500,1000];
-  // var charator = ['I','V','X','L','C','D','M'];
-
-  // var baseNum = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-  // var charator = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
-
-  var baseNumObj = [
-    {number: 1, roman: 'I'},
-    {number: 4, roman: 'IV'},
-    {number: 5, roman: 'V'},
-    {number: 9, roman: 'IX'},
-    {number: 10, roman: 'X'},
-    {number: 40, roman: 'XL'},
-    {number: 50, roman: 'L'},
-    {number: 90, roman: 'XC'},
-    {number: 100, roman: 'C'},
-    {number: 400, roman: 'CD'},
-    {number: 500, roman: 'D'},
-    {number: 900, roman: 'CM'},
-    {number: 1000, roman: 'M'},
-  ];
+  // var baseNumObj = [
+    // {number: 1, roman: 'I'},
+    // {number: 4, roman: 'IV'},
+    // {number: 5, roman: 'V'},
+    // {number: 9, roman: 'IX'},
+    // {number: 10, roman: 'X'},
+    // {number: 40, roman: 'XL'},
+    // {number: 50, roman: 'L'},
+    // {number: 90, roman: 'XC'},
+    // {number: 100, roman: 'C'},
+    // {number: 400, roman: 'CD'},
+    // {number: 500, roman: 'D'},
+    // {number: 900, roman: 'CM'},
+    // {number: 1000, roman: 'M'},
+  // ];
 
   var obj = {
-    convert: function(num) {
-      var returnNum = '';
-      for (var i = 0; i < baseNumObj.length; i++) {
-        if (baseNumObj[i].number == num) {
-          return baseNumObj[i].roman;
-        }
-      }
+     convert: function(num) {
+       return "I";
+      // var returnNum = '';
+      // for (var i = 0; i < baseNumObj.length; i++) {
+        // if (baseNumObj[i].number == num) {
+          // return baseNumObj[i].roman;
+        // }
+      // }
 
       // if (i === baseNum.length) {
         // var index = 0;
--- test.js
+++ test.js
@@ -6,35 +6,43 @@ test("1 should be I", function() {
   ok("I" === roman, "1 should be I, current: " + roman);
 });
 
-test("5 should be V", function() {
-  var romansNumber = new RomansNumber();
-  var roman = romansNumber.convert(5);
-  ok("V" === roman, "5 should be V, current: " + roman);
-});
+// test("2 should be II", function() {
+  // var romansNumber = new RomansNumber();
+  // var roman = romansNumber.convert(1);
+  // ok("II" === roman, "2 should be II, current: " + roman);
+// });
+// test("5 should be V", function() {
+  // var romansNumber = new RomansNumber();
+  // var roman = romansNumber.convert(5);
+  // ok("V" === roman, "5 should be V, current: " + roman);
+// });
+//
+// test("10 should be X", function() {
+  // var romansNumber = new RomansNumber();
+  // var roman = romansNumber.convert(10);
+  // ok("X" === roman, "10 should be X, current: " + roman);
+// });
+//
+// test("4 should be IV", function() {
+  // var romansNumber = new RomansNumber();
+  // var roman = romansNumber.convert(4);
+  // ok("IV" === roman, "4 should be IV, current: " + roman);
+// });
+//
+// test("40 should be XL", function() {
+  // var romansNumber = new RomansNumber();
+  // var roman = romansNumber.convert(40);
+  // ok("XL" === roman, "40 should be XL, current: " + roman);
+// });
+//
+// test("400 should be CD", function() {
+  // var romansNumber = new RomansNumber();
+  // var roman = romansNumber.convert(400);
+  // ok("CD" === roman, "400 should be CD, current: " + roman);
+// });
 
-test("10 should be X", function() {
-  var romansNumber = new RomansNumber();
-  var roman = romansNumber.convert(10);
-  ok("X" === roman, "10 should be X, current: " + roman);
-});
+// 500, 900, 1000...
 
-test("4 should be IV", function() {
-  var romansNumber = new RomansNumber();
-  var roman = romansNumber.convert(4);
-  ok("IV" === roman, "4 should be IV, current: " + roman);
-});
-
-test("40 should be XL", function() {
-  var romansNumber = new RomansNumber();
-  var roman = romansNumber.convert(40);
-  ok("XL" === roman, "40 should be XL, current: " + roman);
-});
-
-test("400 should be CD", function() {
-  var romansNumber = new RomansNumber();
-  var roman = romansNumber.convert(400);
-  ok("CD" === roman, "400 should be CD, current: " + roman);
-});
 
 // test("90 should be XC", function() {
   // var romansNumber = new RomansNumber();
